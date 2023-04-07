8e033dabba7ebf39b6b9978434cac5b444e84cf4
Roman <72063601+foxtrot-dfm1@users.noreply.github.com>
2023-04-07 16:32:36 +0200

    Omit prenotazioni folder from navigation + tests (#4)
    * Omit prenotazioni folder from navigation + tests
    
    * Blacked
    
    ---------
    
    Co-authored-by: Roman <roman.kysil@redturtle.it>

--- src/design/plone/ioprenoto/tests/test_PrenotazioniFolder_ct.py
+++ src/design/plone/ioprenoto/tests/test_PrenotazioniFolder_ct.py
@@ -3,6 +3,9 @@
 from design.plone.ioprenoto.testing import (
     DESIGN_PLONE_IOPRENOTO_INTEGRATION_TESTING,
 )
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import setRoles
+
 from plone import api
 
 import unittest
@@ -14,6 +17,15 @@ class TestPrenotazioniFolder(unittest.TestCase):
     def setUp(self):
         """Custom shared utility setup for tests."""
         self.portal = self.layer["portal"]
+        self.request = self.layer["request"]
+
+        setRoles(self.portal, TEST_USER_ID, ["Manager"])
+
+        self.prenotazioni_folder = api.content.create(
+            type="PrenotazioniFolder",
+            title="Prenotazioni Folder",
+            container=self.portal,
+        )
 
     def test_behaviors_enabled_for_persona(self):
         """Test that PrenotazioniFolder has all the behaviors
@@ -27,3 +39,6 @@ class TestPrenotazioniFolder(unittest.TestCase):
             "design.plone.ioprenoto.behaviors.orario_di_apertura",
         ):
             self.assertIn(behavior, portal_types["PrenotazioniFolder"].behaviors)
+
+    def test_exclude_from_nav(self):
+        self.assertTrue(self.prenotazioni_folder.exclude_from_nav)
