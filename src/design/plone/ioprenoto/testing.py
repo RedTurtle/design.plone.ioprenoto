9940b2e2c693e330a66ad25942b6940eaca4a834
Andrea Cecchi <andrea.cecchi85@gmail.com>
2023-05-26 23:17:44 +0200

    fix tests

--- src/design/plone/ioprenoto/testing.py
+++ src/design/plone/ioprenoto/testing.py
@@ -1,72 +1,27 @@
 # -*- coding: utf-8 -*-
-from design.plone.contenttypes.testing import DesignPloneContenttypesRestApiLayer
-from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
+from design.plone.policy.testing import DesignPlonePolicyRestApiLayer
+from design.plone.policy.testing import DesignPlonePolicyLayer
 from plone.app.testing import applyProfile
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
-from plone.app.testing import PLONE_FIXTURE
-from plone.app.testing import PloneSandboxLayer
 from plone.testing import z2
 
 
-class DesignPloneIoprenotoLayer(PloneSandboxLayer):
-    defaultBases = (PLONE_FIXTURE,)
+import design.plone.ioprenoto
+import redturtle.prenotazioni
+import collective.contentrules.mailfromfield
 
+
+class DesignPloneIoprenotoLayer(DesignPlonePolicyLayer):
     def setUpZope(self, app, configurationContext):
-        # Load any other ZCML that is required for your tests.
-        # The z3c.autoinclude feature is disabled in the Plone fixture base
-        # layer.
-        import collective.contentrules.mailfromfield
-        import collective.taxonomy
-        import collective.venue
-        import collective.z3cform.datagridfield
-        import design.plone.contenttypes
-
-        # required to install redturtle.prenotazioni and design.plone.ioprenoto in test
-        import design.plone.policy
-        import eea.api.taxonomy
-        import plone.app.caching
-        import plone.app.dexterity
-        import redturtle.bandi
-        import redturtle.prenotazioni
-        import redturtle.volto
-
         super().setUpZope(app, configurationContext)
 
-        self.loadZCML(package=plone.app.dexterity)
-        self.loadZCML(package=design.plone.contenttypes)
-        self.loadZCML(package=collective.contentrules.mailfromfield)
-        self.loadZCML(package=redturtle.bandi)
-        self.loadZCML(package=collective.venue)
-        self.loadZCML(package=redturtle.volto)
-        self.loadZCML(package=eea.api.taxonomy)
-        self.loadZCML(package=collective.z3cform.datagridfield)
-        self.loadZCML(package=collective.taxonomy)
         self.loadZCML(package=redturtle.prenotazioni)
-
-        import plone.restapi
-
-        self.loadZCML(package=plone.restapi)
+        self.loadZCML(package=collective.contentrules.mailfromfield)
         self.loadZCML(package=design.plone.ioprenoto)
 
     def setUpPloneSite(self, portal):
         super().setUpPloneSite(portal)
-
-        applyProfile(portal, "plone.app.caching:default")
-
-        # required by design.plone.contenttypes profile
-        applyProfile(portal, "redturtle.bandi:default")
-        applyProfile(portal, "collective.venue:default")
-        applyProfile(portal, "redturtle.volto:default")
-        applyProfile(portal, "collective.taxonomy:default")
-        applyProfile(portal, "eea.api.taxonomy:default")
-        applyProfile(portal, "collective.z3cform.datagridfield:default")
-
-        # used by the current product
-        applyProfile(portal, "design.plone.contenttypes:default")
-
-        # required by design.plone.ioprenoto
-        applyProfile(portal, "redturtle.prenotazioni:default")
 
         applyProfile(portal, "design.plone.ioprenoto:default")
 
@@ -86,23 +41,13 @@ DESIGN_PLONE_IOPRENOTO_FUNCTIONAL_TESTING = FunctionalTesting(
 )
 
 
-DESIGN_PLONE_IOPRENOTO_ACCEPTANCE_TESTING = FunctionalTesting(
-    bases=(
-        DESIGN_PLONE_IOPRENOTO_FIXTURE,
-        REMOTE_LIBRARY_BUNDLE_FIXTURE,
-        z2.ZSERVER_FIXTURE,
-    ),
-    name="DesignPloneIoprenotoLayer:AcceptanceTesting",
-)
-
-
-class DesignPloneContenttypesRestApiLayer(
-    DesignPloneIoprenotoLayer, DesignPloneContenttypesRestApiLayer
+class DesignPloneIopranotoRestApiLayer(
+    DesignPloneIoprenotoLayer, DesignPlonePolicyRestApiLayer
 ):
     pass
 
 
-DESIGN_PLONE_IOPRENOTO_API_FIXTURE = DesignPloneContenttypesRestApiLayer()
+DESIGN_PLONE_IOPRENOTO_API_FIXTURE = DesignPloneIopranotoRestApiLayer()
 DESIGN_PLONE_IOPRENOTO_API_INTEGRATION_TESTING = IntegrationTesting(
     bases=(DESIGN_PLONE_IOPRENOTO_API_FIXTURE,),
     name="DesignPloneIoprenotoRestApiLayer:Integration",
---
+++ src/design/plone/ioprenoto/tests/test_stringinterp_overrides.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+from datetime import date
+from plone import api
+from plone.app.testing import (
+    SITE_OWNER_NAME,
+    SITE_OWNER_PASSWORD,
+    TEST_USER_ID,
+    setRoles,
+)
+from plone.restapi.testing import RelativeSession
+from design.plone.ioprenoto.testing import DESIGN_PLONE_IOPRENOTO_FUNCTIONAL_TESTING
+from datetime import datetime
+from plone.restapi.serializer.converters import json_compatible
+from redturtle.prenotazioni.adapters.booker import IBooker
+from zope.component import getAdapter
+from plone.stringinterp.interfaces import IStringSubstitution
+
+import calendar
+import unittest
+import transaction
+
+
+class TestStringinterpOverrides(unittest.TestCase):
+    layer = DESIGN_PLONE_IOPRENOTO_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.app = self.layer["app"]
+        self.portal = self.layer["portal"]
+        self.portal_url = self.portal.absolute_url()
+        setRoles(self.portal, TEST_USER_ID, ["Manager"])
+
+        self.folder_prenotazioni = api.content.create(
+            container=self.portal,
+            type="PrenotazioniFolder",
+            title="Prenota foo",
+            description="",
+            daData=date.today(),
+            week_table=[
+                {
+                    "day": "Lunedì",
+                    "morning_start": "0700",
+                    "morning_end": "1000",
+                    "afternoon_start": None,
+                    "afternoon_end": None,
+                },
+                {
+                    "day": "Martedì",
+                    "morning_start": None,
+                    "morning_end": None,
+                    "afternoon_start": None,
+                    "afternoon_end": None,
+                },
+                {
+                    "day": "Mercoledì",
+                    "morning_start": None,
+                    "morning_end": None,
+                    "afternoon_start": None,
+                    "afternoon_end": None,
+                },
+                {
+                    "day": "Giovedì",
+                    "morning_start": None,
+                    "morning_end": None,
+                    "afternoon_start": None,
+                    "afternoon_end": None,
+                },
+                {
+                    "day": "Venerdì",
+                    "morning_start": None,
+                    "morning_end": None,
+                    "afternoon_start": None,
+                    "afternoon_end": None,
+                },
+                {
+                    "day": "Sabato",
+                    "morning_start": None,
+                    "morning_end": None,
+                    "afternoon_start": None,
+                    "afternoon_end": None,
+                },
+                {
+                    "day": "Domenica",
+                    "morning_start": None,
+                    "morning_end": None,
+                    "afternoon_start": None,
+                    "afternoon_end": None,
+                },
+            ],
+            booking_types=[
+                {"name": "Type A", "duration": "30"},
+            ],
+            gates=["Gate A"],
+        )
+
+        year = api.content.create(
+            container=self.folder_prenotazioni, type="PrenotazioniYear", title="Year"
+        )
+        week = api.content.create(container=year, type="PrenotazioniWeek", title="Week")
+        self.day_folder = api.content.create(
+            container=week, type="PrenotazioniDay", title="Day"
+        )
+
+        booker = IBooker(self.folder_prenotazioni)
+        self.prenotazione = booker.create(
+            {
+                "booking_date": datetime.now(),
+                "booking_type": "Type A",
+                "title": "foo",
+            }
+        )
+
+        transaction.commit()
+
+    def test_booking_print_url_override(
+        self,
+    ):
+        self.assertIn(
+            "prenotazione-appuntamenti-uffici",
+            getAdapter(self.prenotazione, IStringSubstitution, "booking_print_url")(),
+        )
+
+    def test_booking_print_url_with_delete_token_override(
+        self,
+    ):
+        self.assertIn(
+            "prenotazione-appuntamenti-uffici",
+            getAdapter(
+                self.prenotazione,
+                IStringSubstitution,
+                "booking_print_url_with_delete_token",
+            )(),
+        )
