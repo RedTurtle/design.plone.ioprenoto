7eb5abc9be22681b098a04d19b9a6858d19cf6b2
Mauro Amico <mauro.amico@gmail.com>
2023-07-05 15:38:44 +0200

    fix: booking_url

--- src/design/plone/ioprenoto/restapi/services/bookings/search.py
+++ src/design/plone/ioprenoto/restapi/services/bookings/search.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
+from plone import api
 from redturtle.prenotazioni.restapi.services.bookings.search import (
     BookingsSearch as BookingsSearchBase,
 )  # noqa: E501
 from zope.interface import implementer
 from zope.publisher.interfaces import IPublishTraverse
-from plone import api
 
 
+# TODO: in alternativa si poteva sovrascrivere il serializer
 @implementer(IPublishTraverse)
 class BookingsSearch(BookingsSearchBase):
     def reply(self):
@@ -16,5 +17,5 @@ class BookingsSearch(BookingsSearchBase):
         )
         base_url = f"{base_url}/prenotazione-appuntamenti-uffici"
         for item in response.get("items") or []:
-            item["url"] = f"{base_url}?booking_id={item['booking_id']}"
+            item["booking_url"] = f"{base_url}?booking_id={item['booking_id']}"
         return response
