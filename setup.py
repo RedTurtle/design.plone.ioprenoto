85bed1a84ccfa7398feb106a33855002c009cbea
Mauro Amico <mauro.amico@gmail.com>
2023-08-04 01:35:11 +0200

    req

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
         "z3c.jbot",
         "plone.api>=1.8.4",
         "plone.app.dexterity",
-        "redturtle.prenotazioni>=2.0.0.dev1",
+        "redturtle.prenotazioni>=2.0.0.dev4",
         "design.plone.policy",
     ],
     extras_require={
