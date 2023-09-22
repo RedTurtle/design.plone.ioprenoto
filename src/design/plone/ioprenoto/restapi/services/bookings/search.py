0b425ce18981b8fd5a0c841c3a92f1c4dbe35855
Mauro Amico <mauro.amico@gmail.com>
2023-09-22 13:31:31 +0200

    booking override

--- src/design/plone/ioprenoto/restapi/services/bookings/search.py
+++ src/design/plone/ioprenoto/restapi/services/bookings/search.py
@@ -2,6 +2,7 @@
 from plone import api
 from redturtle.prenotazioni.restapi.services.bookings.search import (
     BookingsSearch as BookingsSearchBase,
+    BookingsSearchFolder as BookingsSearchFolderBase,
 )  # noqa: E501
 from zope.interface import implementer
 from zope.publisher.interfaces import IPublishTraverse
@@ -11,7 +12,21 @@ from zope.publisher.interfaces import IPublishTraverse
 @implementer(IPublishTraverse)
 class BookingsSearch(BookingsSearchBase):
     def reply(self):
-        response = super(BookingsSearch, self).reply()
+        response = super().reply()
+        base_url = api.portal.get_registry_record(
+            name="volto.frontend_domain", default=""
+        )
+        base_url = f"{base_url}/prenotazione-appuntamenti-uffici"
+        for item in response.get("items") or []:
+            item["booking_url"] = f"{base_url}?booking_id={item['booking_id']}"
+        return response
+
+
+# TODO: in alternativa si poteva sovrascrivere il serializer
+@implementer(IPublishTraverse)
+class BookingsSearchFolder(BookingsSearchFolderBase):
+    def reply(self):
+        response = super().reply()
         base_url = api.portal.get_registry_record(
             name="volto.frontend_domain", default=""
         )
