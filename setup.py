224d3b67e5c2e755e7ec840080c44a71cfedafe1
Mauro Amico <mauro.amico@gmail.com>
2023-09-22 16:06:02 +0200

    Preparing release 1.1.6

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="design.plone.ioprenoto",
-    version="1.1.6.dev0",
+    version="1.1.6",
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
