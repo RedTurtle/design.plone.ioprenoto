d08aaf695d52170a34c450ddcbfc92cd34ab0d76
Andrea Cecchi <andrea.cecchi85@gmail.com>
2023-09-25 13:55:44 +0200

    Back to development: 1.1.8

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="design.plone.ioprenoto",
-    version="1.1.7",
+    version="1.1.8.dev0",
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
