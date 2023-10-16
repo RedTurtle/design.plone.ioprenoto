3c6414363ca6611d823c78ca1b3e37c2a4abe274
Roman <roman.kysil@redturtle.it>
2023-10-16 09:59:56 +0200

    Back to development: 1.1.11

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="design.plone.ioprenoto",
-    version="1.1.10",
+    version="1.1.11.dev0",
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
